9206d5f9452f514358a7c6b2d334faea91366b70
Joao victor <joaoparronchi04@gmail.com>
2026-08-10 12:47:31 -0300

    função meio dinamica de idade

--- dist/utils.ts/idade.js
+++ dist/utils.ts/idade.js
@@ -1,6 +1,17 @@
 export function idade() {
-    const anoAtual = new Date().getFullYear();
     const nasc = 2003;
-    return anoAtual - nasc;
+    const data = new Date();
+    const anoAtual = data.getFullYear() - 1;
+    const mesAtual = data.getMonth() + 2;
+    if (mesAtual == 11) {
+        console.log("voce fez aniversario esse mês, vou acrescentar 1 ano na sua idade, meus parabens");
+        anoAtual + 1;
+    }
+    const idade = anoAtual - nasc;
+    const meses = (idade * 12 + mesAtual) % 12; //pegando resto que são os meses
+    const anos = Math.trunc((idade * 12 + mesAtual) / 12); //pegando resto que são os meses
+    console.log(`eu tenho ${anos} anos e ${meses} meses`);
+    return anos;
 }
+//refazer com precisão dos dias tambem
 //# sourceMappingURL=idade.js.map
